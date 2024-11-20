a03cd35302c9745ced44d669465ccc0be0f1170c
Anna Levchenko <83172770+annalevchenko04@users.noreply.github.com>
2024-11-20 13:44:17 +0200

    Add files via upload

--- SQL_SELECT_Anna_Levchenko_HW/part2_task2.sql
+++ SQL_SELECT_Anna_Levchenko_HW/part2_task2.sql
@@ -1,15 +1,14 @@
--- Retrieve the top 5 most rented movies and their expected audience age
 SELECT 
     f.title,  -- Movie title
     COUNT(r.rental_id) AS rental_count,  -- Total number of rentals
     f.rating,  -- Movie rating
     -- Determine expected audience age based on the rating system
     CASE 
-        WHEN LOWER(f.rating) = LOWER('G') THEN 'All ages'
-        WHEN LOWER(f.rating) = LOWER('PG') THEN ' Parental guidance suggested: Some material may not be suitable for children.'
-        WHEN LOWER(f.rating) = LOWER('PG-13') THEN 'Parents strongly cautioned, suitable for ages 13 and up'
-        WHEN LOWER(f.rating) = LOWER('R') THEN 'Restricted, suitable for ages 17 and up'
-        WHEN LOWER(f.rating) = LOWER('NC-17') THEN 'Adults only, suitable for ages 18 and up'
+        WHEN LOWER(CAST(f.rating AS TEXT)) = LOWER('G') THEN 'All ages'
+        WHEN LOWER(CAST(f.rating AS TEXT)) = LOWER('PG') THEN 'Parental guidance suggested: Some material may not be suitable for children.'
+        WHEN LOWER(CAST(f.rating AS TEXT)) = LOWER('PG-13') THEN 'Parents strongly cautioned, suitable for ages 13 and up'
+        WHEN LOWER(CAST(f.rating AS TEXT)) = LOWER('R') THEN 'Restricted, suitable for ages 17 and up'
+        WHEN LOWER(CAST(f.rating AS TEXT)) = LOWER('NC-17') THEN 'Adults only, suitable for ages 18 and up'
         ELSE 'Unknown age rating'  -- In case there are unrated films
     END AS expected_age
 FROM public.rental AS r  -- Added schema prefix

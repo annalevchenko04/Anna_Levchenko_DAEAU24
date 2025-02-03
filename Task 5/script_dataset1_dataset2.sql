a39c91bd8604dcce19883b5addc679be9b190336
Anna Levchenko <83172770+annalevchenko04@users.noreply.github.com>
2025-02-03 18:27:39 +0200

    Add files via upload

---
+++ Task 5/script_dataset1_dataset2.sql
@@ -0,0 +1,179 @@
+CREATE SCHEMA IF NOT EXISTS sa_v_sales_data_1;
+
+CREATE SCHEMA IF NOT EXISTS sa_v_sales_data_2;
+
+CREATE EXTENSION IF NOT EXISTS file_fdw;
+
+DROP SERVER IF EXISTS sales_server CASCADE;
+
+CREATE SERVER sales_server FOREIGN DATA WRAPPER file_fdw;
+
+DROP FOREIGN TABLE IF EXISTS sa_v_sales_data_1.ext_sales;
+
+CREATE FOREIGN TABLE sa_v_sales_data_1.ext_sales (
+    vin VARCHAR(1000),
+    city VARCHAR(1000),
+    state VARCHAR(1000),
+    postal_code VARCHAR(1000),
+    model_year VARCHAR(1000),
+    make VARCHAR(1000),
+    model VARCHAR(1000),
+    electric_vehicle_type VARCHAR(1000),
+    cafv_eligibility VARCHAR(1000),
+    electric_range VARCHAR(1000),
+    transactionid VARCHAR(1000),
+    selling_price VARCHAR(1000),
+    date VARCHAR(1000),
+    customer_passport VARCHAR(1000),
+    customer_name VARCHAR(1000),
+    gender VARCHAR(1000),
+    employee_id VARCHAR(1000),
+    employee_name VARCHAR(1000),
+    employee_phone VARCHAR(1000),
+    channel_id VARCHAR(1000),
+    channel_type VARCHAR(1000),
+    channel_description VARCHAR(1000)
+) SERVER sales_server OPTIONS (
+    filename 'C:/Program Files/PostgreSQL/Electric_Vehicle_Population_Data_1.csv', 
+    format 'csv', 
+    header 'true', 
+    delimiter ','
+);
+
+DROP TABLE IF EXISTS  sa_v_sales_data_1.src_sales;
+
+CREATE TABLE IF NOT EXISTS sa_v_sales_data_1.src_sales (
+    vin VARCHAR(1000),
+    city VARCHAR(1000),
+    state VARCHAR(1000),
+    postal_code VARCHAR(1000),
+    model_year VARCHAR(1000),
+    make VARCHAR(1000),
+    model VARCHAR(1000),
+    electric_vehicle_type VARCHAR(1000),
+    cafv_eligibility VARCHAR(1000),
+    electric_range VARCHAR(1000),
+    transactionid VARCHAR(1000),
+    selling_price VARCHAR(1000),
+    date VARCHAR(1000),
+    customer_passport VARCHAR(1000),
+    customer_name VARCHAR(1000),
+    gender VARCHAR(1000),
+    employee_id VARCHAR(1000),
+    employee_name VARCHAR(1000),
+    employee_phone VARCHAR(1000),
+    channel_id VARCHAR(1000),
+    channel_type VARCHAR(1000),
+    channel_description VARCHAR(1000)
+);
+
+INSERT INTO sa_v_sales_data_1.src_sales
+SELECT DISTINCT 
+    vin::VARCHAR(1000),
+    city::VARCHAR(1000),
+    state::VARCHAR(1000),
+    postal_code::VARCHAR(1000),
+    model_year::VARCHAR(1000),
+    make::VARCHAR(1000),
+    model::VARCHAR(1000),
+    electric_vehicle_type::VARCHAR(1000),
+    cafv_eligibility::VARCHAR(1000),
+    electric_range::VARCHAR(1000),
+    transactionid::VARCHAR(1000),
+    selling_price::VARCHAR(1000),
+    date::VARCHAR(1000),
+    customer_passport::VARCHAR(1000),
+    customer_name::VARCHAR(1000),
+    gender::VARCHAR(1000),
+    employee_id::VARCHAR(1000),
+    employee_name::VARCHAR(1000),
+    employee_phone::VARCHAR(1000),
+    channel_id::VARCHAR(1000),
+    channel_type::VARCHAR(1000),
+    channel_description::VARCHAR(1000)
+FROM sa_v_sales_data_1.ext_sales;
+
+DROP FOREIGN TABLE IF EXISTS sa_v_sales_data_2.ext_sales;
+
+CREATE FOREIGN TABLE sa_v_sales_data_2.ext_sales (
+	vin VARCHAR(1000),
+    city VARCHAR(1000),
+    state VARCHAR(1000),
+    postal_code VARCHAR(1000),
+    model_year VARCHAR(1000),
+    make VARCHAR(1000),
+    model VARCHAR(1000),
+    electric_vehicle_type VARCHAR(1000),
+    cafv_eligibility VARCHAR(1000),
+    electric_range VARCHAR(1000),
+    transactionid VARCHAR(1000),
+    selling_price VARCHAR(1000),
+    date VARCHAR(1000),
+    transmission VARCHAR(1000),
+    color VARCHAR(1000),
+    customer_name VARCHAR(1000),
+    customer_age VARCHAR(1000),
+    customer_id VARCHAR(1000),
+    payment_method VARCHAR(1000),
+    employee_id VARCHAR(1000),
+    employee_name VARCHAR(1000),
+    employee_email VARCHAR(1000)
+) SERVER sales_server OPTIONS (
+    filename 'C:/Program Files/PostgreSQL/Electric_Vehicle_Population_Data_2.csv', 
+    format 'csv', 
+    header 'true', 
+    delimiter ','
+);
+
+DROP TABLE IF EXISTS  sa_v_sales_data_2.src_sales;
+
+CREATE TABLE IF NOT EXISTS sa_v_sales_data_2.src_sales (
+    vin VARCHAR(1000),
+    city VARCHAR(1000),
+    state VARCHAR(1000),
+    postal_code VARCHAR(1000),
+    model_year VARCHAR(1000),
+    make VARCHAR(1000),
+    model VARCHAR(1000),
+    electric_vehicle_type VARCHAR(1000),
+    cafv_eligibility VARCHAR(1000),
+    electric_range VARCHAR(1000),
+    transactionid VARCHAR(1000),
+    selling_price VARCHAR(1000),
+    date VARCHAR(1000),
+    transmission VARCHAR(1000),
+    color VARCHAR(1000),
+    customer_name VARCHAR(1000),
+    customer_age VARCHAR(1000),
+    customer_id VARCHAR(1000),
+    payment_method VARCHAR(1000),
+    employee_id VARCHAR(1000),
+    employee_name VARCHAR(1000),
+    employee_email VARCHAR(1000)
+);
+
+INSERT INTO sa_v_sales_data_2.src_sales
+SELECT DISTINCT 
+    vin::VARCHAR(1000),
+    city::VARCHAR(1000),
+    state::VARCHAR(1000),
+    postal_code::VARCHAR(1000),
+    model_year::VARCHAR(1000),
+    make::VARCHAR(1000),
+    model::VARCHAR(1000),
+    electric_vehicle_type::VARCHAR(1000),
+    cafv_eligibility::VARCHAR(1000),
+    electric_range::VARCHAR(1000),
+    transactionid::VARCHAR(1000),
+    selling_price::VARCHAR(1000),
+    date::VARCHAR(1000),
+    transmission::VARCHAR(1000),
+    color::VARCHAR(1000),
+    customer_name::VARCHAR(1000),
+    customer_age::VARCHAR(1000),
+    customer_id::VARCHAR(1000),
+    payment_method::VARCHAR(1000),
+    employee_id::VARCHAR(1000),
+    employee_name::VARCHAR(1000),
+    employee_email::VARCHAR(1000)
+FROM sa_v_sales_data_2.ext_sales;
